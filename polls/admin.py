4fe457b7200de9293cd84cb0ef39b01593eced45
provedov <mixasmitti@gmail.com>
2023-03-27 16:42:09 +0300

    Realize

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
-
-admin.site.register(Question)
+from .models import Question,Choice
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,{'fields': ['question_text']}),
+        ('Дата создания', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    search_fields = ['question_text']
+admin.site.register(Question, QuestionAdmin)
